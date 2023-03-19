36389c619880898e1405e80c4d794e756e573aef
Rohit Nandi <rohitnandibmp@gmail.com>
2023-03-19 22:30:44 +0530

    commit before configuring the pie chart

--- src/App.js
+++ src/App.js
@@ -3,16 +3,27 @@ import Navbar from "./Components/Navbar";
 import Result from "./Components/Result";
 import SliderSelect from "./Components/SliderSelect";
 import TenureSelect from "./Components/TenureSelect";
+import { useState } from "react";
 
 
 function App() {
+
+  const [data, setData]=useState({
+    productValue: 10000,
+    downPayment: 1000,
+    loanAmount: 9000,
+    loanTerm: 12,
+    interestRate: 12
+  });
+
+
   return (
     <div className="App">
       <Navbar></Navbar>
       <Container maxWidth="xl" sx={{ mt: 4 }}>
         <Grid container spacing={5}>
           <Grid item xs={12} md={6}>
-            <SliderSelect></SliderSelect>
+            <SliderSelect data={data} setData={setData}></SliderSelect>
             <TenureSelect></TenureSelect>
           </Grid>
           <Grid item xs={12} md={6}>
--- src/Components/SliderSelect.js
+++ src/Components/SliderSelect.js
@@ -1,13 +1,72 @@
+import { Stack } from '@mui/material'
 import React from 'react'
 import SliderComponent from './common/SliderComponent'
 
 
-const SliderSelect = () => {
+const SliderSelect = ({data,setData}) => {
+  let unit='₹';
   return (
     <div className=''>
-        <SliderComponent></SliderComponent>
-        <SliderComponent></SliderComponent>
-        <SliderComponent></SliderComponent>
+      <Stack my={1.4}>
+        <SliderComponent 
+          min={0} 
+          max={100000} 
+          deafultvalue={data.productValue} 
+          step={1000} 
+          unit={unit} 
+          label={'Product Value'} 
+          amount={data.productValue}
+          value={data.productValue}
+          onChange={(e, value)=> setData({
+            ...data,
+            loanAmount: value-data.downPayment,
+            productValue: value
+          })}>
+        </SliderComponent>
+        <SliderComponent 
+          min={0} 
+          max={data.productValue} 
+          deafultvalue={data.downPayment} 
+          step={500}
+          label={'Down Payment'} 
+          amount={data.downPayment}
+          value={data.downPayment}
+          onChange={(e, value)=> setData({
+            ...data,
+            loanAmount: data.productValue-value,
+            downPayment: value
+          })}>
+        </SliderComponent>
+        <SliderComponent 
+          min={0} 
+          max={data.productValue} 
+          deafultvalue={data.loanAmount} 
+          step={500} 
+          unit={unit} 
+          label={'Loan Amount'} 
+          amount={data.loanAmount}
+          value={data.loanAmount}
+          onChange={(e, value)=> setData({
+            ...data,
+            downPayment: data.productValue-value,
+            loanAmount: value
+          })}>
+        </SliderComponent>
+        <SliderComponent 
+          min={0} 
+          max={30} 
+          deafultvalue={data.interestRate} 
+          step={0.5} 
+          unit={'%'} 
+          label={'Interest Rate'} 
+          amount={data.interestRate}
+          value={data.interestRate}
+          onChange={(e, value)=> setData({
+            ...data,
+            interestRate: value
+          })}>
+        </SliderComponent>
+      </Stack>
     </div>
   )
 }
